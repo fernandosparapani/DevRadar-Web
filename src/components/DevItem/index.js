2a3dce6edcdc5de303c7453c907944f88f60918c
Fernando Sparapani <luisfernandospda@gmail.com>
2020-01-15 22:20:54 -0300

    Terceira aula da semana OmniStack 10.0

---
+++ src/components/DevItem/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+import './styles.css';
+
+function DevItem({ dev }) {
+
+  return (
+
+    <li className="dev-item">
+      <header>
+        <img src={dev.avatar_url} alt={dev.name} />
+        <div className="user-info">
+          <strong>{dev.name}</strong>
+          <span>{dev.techs.join(', ')}</span>
+        </div>
+      </header>
+      <p>{dev.bio}</p>
+
+      <a href={`https://github.com/${dev.github_username}`}>Acessar pefil no Github</a>
+      <div className="user-functions">
+          {/* <span>Editar</span>
+          <span>Remover</span> */}
+        </div>     
+    </li>
+  
+  );
+}
+
+export default DevItem;
